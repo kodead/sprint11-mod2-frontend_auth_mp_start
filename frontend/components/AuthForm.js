32d3a932bcc508d592a7de41c7e5b6121d443233
Ahmad Koroma <ahmadkoroma25@outlook.com>
2024-11-01 07:42:16 -0400

    done

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
   const [message, setMessage] = useState('')
-
+  const navigate = useNavigate()
   const toggleFormMode = () => {
     setIsLogin(!isLogin)
     setError('')
@@ -18,6 +19,34 @@ export default function AuthForm() {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value)
   }
+  // Handle submit will be an asynchronous function that takes an event object.
+  const handleSubmit = async (event) => {
+    // We will use the event object to prevent the default behavior of the form on submission.
+    // and we will take the opportunity now to clear any messages from the server
+    event.preventDefault()
+    setError('')
+    setMessage('')
+    try {
+      // declaring a data variable and pull it out of await in axios.post
+      const { data} = await axios.post(
+        //the first argument is the url we are posting to
+        //the last segment depends on whether we are in login mode or in register mode
+        `/api/auth/${isLogin ? 'login' : 'register'}`,
+        {username, password}
+      )
+      if (isLogin) {
+        localStorage.setItem('token', data.token)
+        navigate('/stars')
+      } else {
+        setMessage(data.message)
+      }
+
+    } catch (err) {
+      setError(err?.response?.data?.message ||
+        'An error has occurred. Please try again.'
+      )
+    }
+  }
 
   return (
     <div className="container">
@@ -28,7 +57,7 @@ export default function AuthForm() {
           Switch to {isLogin ? 'Register' : 'Login'}
         </button>
       </h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
